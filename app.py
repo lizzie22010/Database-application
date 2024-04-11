ab50906ad8acc1c16708ff7003e22a2c9181ceeb
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-12 10:26:06 +1200

    12/04/24

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
+# docstring- Lizzie Howe- airplane database application
+# imports
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "select * from fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+# constants and variables
+DATABASE = "fighters.db"
+
+
+# functions
+def print_all_aircraft():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect("fighters.db")
+    cursor = db.cursor()
+    sql = "select * from fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # loop through all the results
+    for fighter in results:
+        print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}{fighter[6]}")
+    # loop finished here
+    db.close()
+
+
+
+# main code
+print_all_aircraft()
